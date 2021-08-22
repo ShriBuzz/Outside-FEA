196f433055f1964b5bcc400dc1657983b9207ad6
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2021-08-22 18:58:46 +0545

    handle resize menu collapse and bg color change

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@ const navBar = document.querySelector('.navbar');
 const navToggle = document.querySelector('.nav-menu');
 const navCollapse = document.querySelector('.navbar-collapse');
 
+window.addEventListener(
+  'resize',
+  function (event) {
+    if (window.innerWidth >= 992) {
+      navBar.classList.remove('nav-collapse-open');
+    } else {
+      if (!navToggle.classList.contains('collapsed')) {
+        navBar.classList.add('nav-collapse-open');
+      } else navBar.classList.remove('nav-collapse-open');
+    }
+  },
+  true
+);
+
 navToggle.addEventListener('click', function () {
   if (navToggle.classList.contains('collapsed')) {
     navBar.classList.remove('nav-collapse-open');
