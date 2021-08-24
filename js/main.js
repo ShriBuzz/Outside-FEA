aa2bde7b8251fe2df086595c6f9fcecc68b39673
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2021-08-24 15:35:33 +0545

    optimize js functions

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
+// navbar logics
 const navBar = document.querySelector('.navbar');
 const navToggle = document.querySelector('.nav-menu');
 const navCollapse = document.querySelector('.navbar-collapse');
 
+var distance = $('.navbar').offset().top,
+  $window = $(window);
+
 window.addEventListener(
   'resize',
-  function (event) {
-    if (window.innerWidth >= 992) {
-      navBar.classList.remove('nav-collapse-open');
-    } else {
-      if (!navToggle.classList.contains('collapsed')) {
-        navBar.classList.add('nav-collapse-open');
-      } else navBar.classList.remove('nav-collapse-open');
-    }
+  function () {
+    distance = $('.navbar').offset().top;
+    $window = $(window);
   },
   true
 );
 
-navToggle.addEventListener('click', function () {
-  if (navToggle.classList.contains('collapsed')) {
-    navBar.classList.remove('nav-collapse-open');
-  } else navBar.classList.add('nav-collapse-open');
+$('.nav-menu').click(function () {
+  $('.navbar').toggleClass('nav-collapse-open nav-padding-remove');
 });
 
-var distance = $('.navbar').offset().top,
-  $window = $(window);
-
 $window.scroll(function () {
-  if ($window.scrollTop() >= distance) {
+  if ($window.scrollTop() > distance) {
     $('.navbar').addClass('nav-collapse-open');
   } else {
-    $('.navbar').removeClass('nav-collapse-open');
+    if (
+      $('#hoverNav').hasClass('hover-active') ||
+      $('.nav-menu').hasClass('nav-collapse-open')
+    ) {
+      return;
+    } else $('.navbar').removeClass('nav-collapse-open');
   }
+});
+
+let hoverToggle = false;
+
+$('#Learn').click(function () {
+  $('.language-section').toggleClass('borderBottom');
+  $('.navbar').toggleClass('nav-collapse-open');
+  $('#Learn').children().toggleClass('active');
+  $('#hoverNav').toggleClass('hover-active');
 });
